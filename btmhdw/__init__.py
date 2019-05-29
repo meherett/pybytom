d2e02bf6d45b1492393851655b881103fb4f7c7f
meherett <meherett@zoho.com>
2019-05-29 12:33:11 +0300

    child public key function updated

--- btmhdw/__init__.py
+++ btmhdw/__init__.py
@@ -206,10 +206,39 @@ class BytomHDWallet:
         child_xprivate = xprivate
         return child_xprivate
 
-    def childXPublicKey(self, xpublic, indexes):
+    def childXPublicKey(self, xpublic=None, indexes=None):
+        if indexes is None:
+            indexes = self.indexes
+        if xpublic:
+            for index in range(len(indexes)):
+                index_bytes = get_bytes(indexes[index])
+                xpublic_bytes = get_bytes(xpublic)
+                I = bytearray(hmac.HMAC(xpublic_bytes[32:],
+                                        b'N' + xpublic_bytes[:32] + index_bytes,
+                                        digestmod=hashlib.sha512).digest())
+
+                Il, Ir = I[:32], I[32:]
+
+                parse_Il = int.from_bytes(Il, 'big')
+                if parse_Il == 0:
+                    raise ValueError("Bad seed, resulting in invalid key!")
+
+                f = bytes(prune_intermediate_scalar(Il))
+                scalar = decodeint(f)
+                F = scalarmultbase(scalar)
+
+                P = decodepoint(xpublic_bytes[:32])
+                P = edwards_add(P, F)
+                public_key = encodepoint(P)
+
+                xpublic_bytes = public_key[:32] + Ir
+                xpublic = xpublic_bytes.hex()
+
+            child_xpublic = xpublic
+            return child_xpublic
         for index in range(len(indexes)):
             index_bytes = get_bytes(indexes[index])
-            xpublic_bytes = get_bytes(xpublic)
+            xpublic_bytes = get_bytes(self.xpublicKey())
             I = bytearray(hmac.HMAC(xpublic_bytes[32:],
                                     b'N' + xpublic_bytes[:32] + index_bytes,
                                     digestmod=hashlib.sha512).digest())
