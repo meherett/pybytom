428321d9482c9bf28eb5576a8ed6be083a161009
meherett <meherett@zoho.com>
2020-02-21 20:49:37 -0800

    signature added

--- bytom/signature.py
+++ bytom/signature.py
@@ -3,7 +3,9 @@
 import hmac
 import hashlib
 
-from .utils import decodeint, encodepoint, scalarmultbase, sc_reduce32, sc_muladd, _verify
+from .utils.utils import \
+    decodeint, encodepoint, scalarmultbase,\
+    sc_reduce32, sc_muladd, _verify
 
 
 def prune_root_scalar(s_str):
@@ -15,23 +17,23 @@ def prune_root_scalar(s_str):
     return s
 
 
-def get_root_xprv(seed_hexstr):
+def get_root_xprivate(seed_hexstr):
     hc_hexstr = hmac.HMAC(b'Root', bytes.fromhex(seed_hexstr), digestmod=hashlib.sha512).hexdigest()
-    root_xprv_hexstr = prune_root_scalar(hc_hexstr[:64]).hex() + hc_hexstr[64:]
-    return root_xprv_hexstr
+    root_xprivate_hexstr = prune_root_scalar(hc_hexstr[:64]).hex() + hc_hexstr[64:]
+    return root_xprivate_hexstr
 
 
-def get_xpub(xprivate):
-    xprv_bytes = bytes.fromhex(xprivate)
-    scalar = decodeint(xprv_bytes[:len(xprv_bytes)//2])
+def get_xpublic(xprivate):
+    xprivate_bytes = bytes.fromhex(xprivate)
+    scalar = decodeint(xprivate_bytes[:len(xprivate_bytes)//2])
     buf = encodepoint(scalarmultbase(scalar))
-    xpub = buf + xprv_bytes[len(xprv_bytes)//2:]
-    xpublic = xpub.hex()
+    xpublic = buf + xprivate_bytes[len(xprivate_bytes)//2:]
+    xpublic = xpublic.hex()
     return xpublic
 
 
 def get_expanded_private_key(xprivate):
-    hc_hexstr = hmac.HMAC(b'Expand', bytes.fromhex(xprivate), digestmod=hashlib.sha512).hexdigest()
+    hc_hexstr = hmac.HMAC(b"Expand", bytes.fromhex(xprivate), digestmod=hashlib.sha512).hexdigest()
     expanded_private_key_hexstr = xprivate[:64] + hc_hexstr[64:]
     return expanded_private_key_hexstr
 
@@ -50,11 +52,11 @@ def prune_intermediate_scalar(f):
     return f
 
 
-def sign(xprivate, message):
-    xprivate = get_expanded_private_key(xprivate)
-    xprv_bytes = bytes.fromhex(xprivate)
+def sign(private, message):
+    private = get_expanded_private_key(private)
+    private_bytes = bytes.fromhex(private)
     message_bytes = bytes.fromhex(message)
-    data_bytes = xprv_bytes[32:64] + message_bytes
+    data_bytes = private_bytes[32:64] + message_bytes
 
     message_digest = hashlib.sha512(data_bytes).digest()
     message_digest = sc_reduce32(message_digest.hex().encode())
@@ -63,16 +65,16 @@ def sign(xprivate, message):
 
     scalar = decodeint(message_digest_reduced)
     encoded_r = encodepoint(scalarmultbase(scalar))
-    xpublic = get_xpub(xprivate)
-    xpub_bytes = bytes.fromhex(xpublic)
-    hram_digest_data = encoded_r + xpub_bytes[:32] + message_bytes
+    public = get_xpublic(private)
+    public_bytes = bytes.fromhex(public)
+    hram_digest_data = encoded_r + public_bytes[:32] + message_bytes
 
     hram_digest = hashlib.sha512(hram_digest_data).digest()
     hram_digest = sc_reduce32(hram_digest.hex().encode())
     hram_digest = bytes.fromhex(hram_digest.decode())
     hram_digest_reduced = hram_digest[0:32]
 
-    sk = xprv_bytes[:32]
+    sk = private_bytes[:32]
     s = sc_muladd(hram_digest_reduced.hex().encode(), sk.hex().encode(), message_digest_reduced.hex().encode())
     s = bytes.fromhex(s.decode())
 
@@ -81,6 +83,5 @@ def sign(xprivate, message):
     return signature
 
 
-def verify(xpublic, message, signature):
-    return _verify(get_public_key(xpublic), signature, message)
-
+def verify(public, message, signature):
+    return _verify(get_public_key(public), signature, message)
