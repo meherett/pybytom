8457fb17c92bb293636f699ed32391a24c5f2d03
meherett <meherett@zoho.com>
2020-02-21 20:53:06 -0800

    Upgrade pybytom

--- setup.py
+++ setup.py
@@ -7,22 +7,21 @@ with open("requirements.txt", "r") as _requirements:
     requirements = list(map(str.strip, _requirements.read().split("\n")))[:-1]
 
 setup(
-    name="btmhdw",
-    version="2.1.3",
-    description="The implementation of Hierarchical Deterministic (HD) wallets generator for Bytom blockchain",
+    name="pybytom",
+    version="0.1.0.dev1",
+    description="Python library with tools for Bytom blockchain.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
     author="Meheret Tesfaye",
     author_email="meherett@zoho.com",
-    url="https://github.com/meherett/btmhdw",
+    url="https://github.com/meherett/pybytom",
     packages=find_packages(),
-    python_requires=">=3.5,<4",
+    python_requires=">=3.6,<4",
     install_requires=requirements,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8"
