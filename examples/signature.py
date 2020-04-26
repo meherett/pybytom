15c8edb14329e45c426c7593f57d324a4c77f835
meherett <meherett@zoho.com>
2020-04-26 20:19:34 +0300

    Update examples.

--- examples/signature.py
+++ examples/signature.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
-from bytom import sign, verify
+from bytom.signature import sign, verify
 
-PRIVATE_KEY = "e07af52746e7cccd0a7d1fba6651a6f474bada481f34b1c5bab5e2d71e36ee515803ee" \
-              "0a6682fb19e279d8f4f7acebee8abd0fc74771c71565f9a9643fd77141"
-
+# Bytom private key and public key
+PRIVATE_KEY = "e07af52746e7cccd0a7d1fba6651a6f474bada481f34b1c5bab5e2d71e36ee51580" \
+              "3ee0a6682fb19e279d8f4f7acebee8abd0fc74771c71565f9a9643fd77141"
 PUBLIC_KEY = "91ff7f525ff40874c4f47f0cab42e46e3bf53adad59adef9558ad1b6448f22e2"
-
+# Message data
 MESSAGE = "1246b84985e1ab5f83f4ec2bdf271114666fd3d9e24d12981a3c861b9ed523c6"
 
-signed = sign(private=PRIVATE_KEY, message=MESSAGE)
-print("Sign:", signed)
-print("Verify:", verify(public=PUBLIC_KEY, message=MESSAGE, signature=signed))
+# Signing message by private key
+signature = sign(private=PRIVATE_KEY, message=MESSAGE)
+print("Signature:", signature)
+
+# Verifying signature by public key
+verified = verify(public=PUBLIC_KEY, signature=signature, message=MESSAGE)
+print("Verified:", verified)
--- examples/wallet.py
+++ examples/wallet.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from bytom import Wallet, PATH, INDEXES
+from bytom.wallet import Wallet, PATH, INDEXES
 
 MNEMONIC = "indicate warm sock mistake code spot acid ribbon sing over taxi toast"
 
-MESSAGE = "1246b84985e1ab5f83f4ec2bdf271114666fd3d9e24d12981a3c861b9ed523c6"
+MESSAGE = "a0841d35364046649ab8fc4af5a6266245890778f6cf7304696c4ab8edd86242"
 
 wallet = Wallet(network="mainnet")\
     .from_mnemonic(mnemonic=MNEMONIC)
@@ -14,13 +14,13 @@ wallet = Wallet(network="mainnet")\
 # wallet.from_indexes(indexes=INDEXES)
 
 # Custom path
-wallet.from_path("m/44/153/1/0/1")
+# wallet.from_path("m/44/153/1/0/1")
 # or index
-# wallet.from_index(44)
-# wallet.from_index(153)
-# wallet.from_index(1)  # account
-# wallet.from_index(0)  # change false(0) or true(1)
-# wallet.from_index(1)  # address
+wallet.from_index(44)
+wallet.from_index(153)
+wallet.from_index(1)  # account
+wallet.from_index(0)  # change false(0) or true(1)
+wallet.from_index(1)  # address
 
 print("Entropy:", wallet.entropy())
 print("Mnemonic:", wallet.mnemonic())
