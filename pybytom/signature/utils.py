be72fc7630872a0dad7cffc869e3aa79485766f7
meherett <meherett@zoho.com>
2020-04-29 22:44:42 +0300

    Update signature utils.py

--- pybytom/signature/utils.py
+++ pybytom/signature/utils.py
@@ -11,21 +11,6 @@ from ..libs.ed25519 import *
 L = 2 ** 252 + 27742317777372353535851937790883648493
 
 
-def prune_root_scalar(s_str):
-    s_bytes = bytes.fromhex(s_str)
-    s = bytearray(s_bytes)
-    s[0] = s[0] & 248
-    s[31] = s[31] & 31  # clear top 3 bits
-    s[31] = s[31] | 64  # set second highest bit
-    return s
-
-
-def get_root_xprivate(seed_hexstr):
-    hc_hexstr = hmac.HMAC(b'Root', bytes.fromhex(seed_hexstr), digestmod=hashlib.sha512).hexdigest()
-    root_xprivate_hexstr = prune_root_scalar(hc_hexstr[:64]).hex() + hc_hexstr[64:]
-    return root_xprivate_hexstr
-
-
 def get_xpublic_key(xprivate):
     xprivate_bytes = bytes.fromhex(xprivate)
     scalar = decodeint(xprivate_bytes[:len(xprivate_bytes)//2])
@@ -46,31 +31,6 @@ def get_public_key(xpublic):
     return public_key_hexstr
 
 
-def prune_intermediate_scalar(f):
-    f = bytearray(f)
-    f[0] = f[0] & 248       # clear bottom 3 bits
-    f[29] = f[29] & 1       # clear 7 high bits
-    f[30] = 0               # clear 8 bits
-    f[31] = 0               # clear 8 bits
-    return f
-
-
-def get_bytes(string):
-    if isinstance(string, bytes):
-        byte = string
-    elif isinstance(string, str):
-        byte = bytes.fromhex(string)
-    else:
-        raise TypeError("Agreement must be either 'bytes' or 'string'!")
-    return byte
-
-
-def _sign(private_key_str, message_str):
-    signing_key = ed25519.SigningKey(bytes.fromhex(private_key_str))
-    signature = signing_key.sign(bytes.fromhex(message_str), encoding='hex')
-    return signature.decode()
-
-
 def _verify(public_key_str, signature_str, message_str):
     result = False
     verifying_key = ed25519.VerifyingKey(public_key_str.encode(), encoding='hex')
@@ -84,21 +44,6 @@ def _verify(public_key_str, signature_str, message_str):
 
 def byte2int(_b):
     return _b
-
-
-def int2byte(i):
-    return bytes(chr(i % 256), encoding="UTF-8")
-
-
-def hmac_sha_512(data, key):
-    digest = hmac.new(key, msg=data, digestmod=hashlib.sha512).digest()
-    return digest
-
-
-def sha_512(data):
-    md = hashlib.sha512()
-    md.update(data)
-    return md.digest()
 
 
 def hex2int(_hex):
