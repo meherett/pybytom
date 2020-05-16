43f1f97d0ee62aa9d07e3bebb295ea0b023fa97a
meherett <meherett@zoho.com>
2020-05-16 16:19:17 +0300

    Added bytom configuration.

--- pybytom/__init__.py
+++ pybytom/__init__.py
@@ -4,7 +4,32 @@ from .wallet import Wallet
 from .signature import sign, verify
 
 
+# Bytom configuration
+configuration = {
+    "mainnet": {
+        "bytom": "http://localhost:9888",
+        "blockmeta": "https://blockmeta.com/api/v2",
+        "blockcenter": "https://bcapi.bystack.com/api/v2/btm"
+    },
+    "solonet": {
+        "bytom": "http://localhost:9888",
+        "blockmeta": "https://blockmeta.com/api/v2",
+        "blockcenter": "https://bcapi.bystack.com/api/v2/btm"
+    },
+    "testnet": {
+        "bytom": "http://localhost:9888",
+        "blockmeta": "https://blockmeta.com/api/wisdom",
+        "blockcenter": "https://bcapi.bystack.com/api/v2/wisdom"
+    },
+    "timeout": 60,
+    "BTM_asset": "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+    "fee": 10_000_000,
+    "confirmations": 1,
+    "sequence": 100
+}
+
 __all__ = [
+    "configuration",
     "Wallet",
     "sign",
     "verify"
