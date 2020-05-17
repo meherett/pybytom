9ff1223be97458aed5782184fcbd8c90b0db4230
meherett <meherett@zoho.com>
2020-05-17 19:26:57 +0300

    Update pybytom.

--- pybytom/__init__.py
+++ pybytom/__init__.py
@@ -1,36 +1,17 @@
 #!/usr/bin/env python3
 
 from .wallet import Wallet
+from .transaction import (
+    Transaction, NormalTransaction, AdvancedTransaction
+)
 from .signature import sign, verify
 
 
-# Bytom configuration
-configuration = {
-    "mainnet": {
-        "bytom": "http://localhost:9888",
-        "blockmeta": "https://blockmeta.com/api/v2",
-        "blockcenter": "https://bcapi.bystack.com/api/v2/btm"
-    },
-    "solonet": {
-        "bytom": "http://localhost:9888",
-        "blockmeta": "https://blockmeta.com/api/v2",
-        "blockcenter": "https://bcapi.bystack.com/api/v2/btm"
-    },
-    "testnet": {
-        "bytom": "http://localhost:9888",
-        "blockmeta": "https://blockmeta.com/api/wisdom",
-        "blockcenter": "https://bcapi.bystack.com/api/v2/wisdom"
-    },
-    "timeout": 60,
-    "BTM_asset": "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-    "fee": 10_000_000,
-    "confirmations": 1,
-    "sequence": 100
-}
-
 __all__ = [
-    "configuration",
     "Wallet",
+    "Transaction",
+    "NormalTransaction",
+    "AdvancedTransaction",
     "sign",
-    "verify"
+    "verify",
 ]
