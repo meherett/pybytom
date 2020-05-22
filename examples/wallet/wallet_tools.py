9c7bf202d60b9794f0f96f797817a70081a0f44e
meherett <meherett@zoho.com>
2020-05-22 14:04:42 +0300

    Update wallet examples.

--- examples/wallet/wallet_tools.py
+++ examples/wallet/wallet_tools.py
@@ -3,7 +3,7 @@
 from pybytom.wallet.tools import (
     path_to_indexes, indexes_to_path, get_xpublic_key, get_expand_xprivate_key,
     get_child_xprivate_key, get_child_xpublic_key, get_private_key,
-    get_public_key, get_program, get_address
+    get_public_key, get_program, get_address, get_vapor_address
 )
 
 
@@ -32,4 +32,6 @@ print("Get Public Key:", get_public_key(xpublic_key=XPUBLIC_KEY))
 
 print("Get Program:", get_program(public_key=get_public_key(xpublic_key=XPUBLIC_KEY)))
 print("Get Address:", get_address(program=get_program(
+    public_key=get_public_key(xpublic_key=XPUBLIC_KEY)), network="mainnet"))
+print("Get Vapor Address:", get_vapor_address(program=get_program(
     public_key=get_public_key(xpublic_key=XPUBLIC_KEY)), network="mainnet"))
