49a6fa332ad18ccc6a74c28eb837ff6420080ba0
meherett <meherett@zoho.com>
2020-05-30 18:45:10 +0300

    Update p2pk.py

--- examples/script/p2pk.py
+++ examples/script/p2pk.py
@@ -11,16 +11,13 @@ NETWORK, PUBLIC_KEY = "mainnet", "5b5a06f6fbcb74b58ebb42293808fec6222234df6c97d7
 # Get public key hash
 public_key_hash = public_key_hash(public_key=PUBLIC_KEY)
 print("Public Key Hash:", public_key_hash)
-
 # Get Pay to Public Key Hash(P2PKH) program
 p2pkh_program = p2pkh_program(public_key_hash=public_key_hash)
 print("P2PKH Program:", p2pkh_program)
-
 # Get Pay to Witness Public Key Hash(P2WPKH) program
 p2wpkh_program = p2wpkh_program(public_key_hash=public_key_hash)
 assert get_program(public_key=PUBLIC_KEY) == p2wpkh_program
 print("P2WPKH Program:", p2wpkh_program)
-
 # Get Pay to Witness Public Key Hash(P2WPKH) address
 p2wpkh_address = p2wpkh_address(public_key_hash=public_key_hash, network=NETWORK)
 assert get_address(program=get_program(public_key=PUBLIC_KEY), network=NETWORK) == p2wpkh_address
