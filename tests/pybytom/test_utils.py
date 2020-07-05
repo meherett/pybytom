f9dd5876170825246409512b91dfd818c8cd746d
meherett <meherett@zoho.com>
2020-07-05 21:01:17 +0300

    Added Bytom address and vapor address checker tester.

--- tests/pybytom/test_utils.py
+++ tests/pybytom/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from pybytom.utils import (
-    generate_mnemonic, generate_entropy, is_mnemonic, get_mnemonic_language
+    generate_mnemonic, generate_entropy, is_mnemonic, get_mnemonic_language, is_address, is_vapor_address
 )
 
 import pytest
@@ -34,3 +34,40 @@ def test_base58():
 
     with pytest.raises(ValueError, match=r"Strength should be one of the following .*"):
         assert generate_mnemonic(strength=129)
+
+
+def test_address_and_vapor_address():
+
+    assert is_address("bm1qp5wf088y45flgapfgzd4ngahpx86luttv8d8a5")
+    assert is_address("bm1qp5wf088y45flgapfgzd4ngahpx86luttv8d8a5", "mainnet")
+    assert is_address("sm1qp5wf088y45flgapfgzd4ngahpx86luttdk8xa6")
+    assert is_address("sm1qp5wf088y45flgapfgzd4ngahpx86luttdk8xa6", "solonet")
+    assert is_address("tm1qp5wf088y45flgapfgzd4ngahpx86luttg3vra9")
+    assert is_address("tm1qp5wf088y45flgapfgzd4ngahpx86luttg3vra9", "testnet")
+
+    assert is_vapor_address("vp1qp5wf088y45flgapfgzd4ngahpx86lutt8xa6cz")
+    assert is_vapor_address("vp1qp5wf088y45flgapfgzd4ngahpx86lutt8xa6cz", "mainnet")
+    assert is_vapor_address("sp1qp5wf088y45flgapfgzd4ngahpx86lutt0rljcf")
+    assert is_vapor_address("sp1qp5wf088y45flgapfgzd4ngahpx86lutt0rljcf", "solonet")
+    assert is_vapor_address("tp1qp5wf088y45flgapfgzd4ngahpx86lutt2y5hck")
+    assert is_vapor_address("tp1qp5wf088y45flgapfgzd4ngahpx86lutt2y5hck", "testnet")
+
+    with pytest.raises(TypeError, match="address must be string format"):
+        assert is_address(1234567890)
+
+    with pytest.raises(TypeError, match="address must be string format"):
+        assert is_vapor_address(1234567890)
+
+    with pytest.raises(TypeError, match="network must be string format"):
+        assert is_address("bm1qp5wf088y45flgapfgzd4ngahpx86luttv8d8a5", 1234567890)
+
+    with pytest.raises(TypeError, match="network must be string format"):
+        assert is_vapor_address("vp1qp5wf088y45flgapfgzd4ngahpx86lutt8xa6cz", 1234567890)
+
+    with pytest.raises(ValueError,
+                       match="invalid network, use only this options mainnet, solonet or testnet networks."):
+        assert is_address("bm1qp5wf088y45flgapfgzd4ngahpx86luttv8d8a5", "network")
+
+    with pytest.raises(ValueError,
+                       match="invalid network, use only this options mainnet, solonet or testnet networks."):
+        assert is_vapor_address("vp1qp5wf088y45flgapfgzd4ngahpx86lutt8xa6cz", "network")
