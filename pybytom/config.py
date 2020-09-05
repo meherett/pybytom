24d9033746905d1f654de9bbe81e9d91ee529ff6
meherett <meherett@zoho.com>
2020-09-05 08:23:32 +0300

    Add: Bytom config.py file

---
+++ pybytom/config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+# Bytom configuration
+def config():
+
+    return {
+        "mainnet": {
+            "bytom": "http://localhost:9888",
+            "blockmeta": "https://blockmeta.com/api/v3",
+            "blockcenter": "https://bcapi.bystack.com/api/v3"
+        },
+        "solonet": {
+            "bytom": "http://localhost:9888",
+            "blockmeta": None,
+            "blockcenter": None
+        },
+        "testnet": {
+            "bytom": "http://localhost:9888",
+            "blockmeta": None,
+            "blockcenter": None
+        },
+        "network": "solonet",  # default network
+        "timeout": 60,
+        "BTM_asset": "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+        "one_BTM": 100_000_000,  # 1 BTM
+        "fee": 10_000_000,  # 0.1 BTM
+        "confirmations": 1
+    }
