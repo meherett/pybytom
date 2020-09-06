43cc470edc879f81bad47143d05ada2e60400b9d
meherett <meherett@zoho.com>
2020-09-06 11:18:44 +0300

    Add: Symbol exception.

--- pybytom/exceptions.py
+++ pybytom/exceptions.py
@@ -74,6 +74,18 @@ class BalanceError(Exception):
         return f"{self.error_message}"
 
 
+class SymbolError(Exception):
+
+    def __init__(self, error_message, error_detail=None):
+        self.error_message = error_message
+        self.error_detail = error_detail
+
+    def __str__(self):
+        if self.error_detail:
+            return f"{self.error_message}, {self.error_detail}"
+        return f"{self.error_message}"
+
+
 class NetworkError(Exception):
 
     def __init__(self, error_message, error_detail=None):
