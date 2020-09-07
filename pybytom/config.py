7c206867744c65f85cb4700b0fb8819d8eb05190
meherett <meherett@zoho.com>
2020-09-07 14:32:28 +0300

    Update: blockcenter versions and add movapi url.

--- pybytom/config.py
+++ pybytom/config.py
@@ -8,22 +8,33 @@ def config():
         "mainnet": {
             "bytom": "http://localhost:9888",
             "blockmeta": "https://blockmeta.com/api/v3",
-            "blockcenter": "https://bcapi.bystack.com/api/v3"
+            "blockcenter": {
+                "version2": "https://bcapi.bystack.com/api/v2/btm",
+                "version3": "https://bcapi.bystack.com/bytom/v3"
+            },
+            "mov": "https://ex.movapi.com/bytom/v3"
         },
         "solonet": {
             "bytom": "http://localhost:9888",
             "blockmeta": None,
-            "blockcenter": None
+            "blockcenter": {
+                "version2": None,
+                "version3": None
+            },
+            "mov": None
         },
         "testnet": {
             "bytom": "http://localhost:9888",
             "blockmeta": None,
-            "blockcenter": None
+            "blockcenter": {
+                "version2": None,
+                "version3": None
+            },
+            "mov": None
         },
         "network": "mainnet",  # default network
         "timeout": 60,
         "BTM_asset": "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-        "one_BTM": 100_000_000,  # 1 BTM
         "fee": 10_000_000,  # 0.1 BTM
         "confirmations": 1
     }
