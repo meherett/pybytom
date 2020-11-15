546608daa9038c13166868be007227c21cdc30b7
meherett <meherett@zoho.com>
2020-11-15 21:50:58 +0300

    Add: Bytom BTM, BTC, ETH and USDT assets.

---
+++ pybytom/assets.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+# Bytom asset
+BTM: str = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+# Bitcoin asset
+BTC: str = "bda946b3110fa46fd94346ce3f05f0760f1b9de72e238835bc4d19f9d64f1742"
+# Ethereum asset
+ETH: str = "78de44ffa1bce37b757c9eae8925b5f199dc4621b412ef0f3f46168865284a93"
+# Tether asset
+USDT: str = "184e1cc4ee4845023888810a79eed7a42c02c544cf2c61ceac05e176d575bd46"
+
+
+__all__:  List[str] = [
+    BTM, BTC, ETH, USDT
+]
