b6a8b68e8bd44ca43766ded9bb2d31413c71dca8
meherett <meherett@zoho.com>
2020-11-15 04:36:43 +0300

    Upgrade: find_smart_contract_utxo and find_p2wsh_utxo functions.

--- pybytom/transaction/tools.py
+++ pybytom/transaction/tools.py
@@ -4,9 +4,14 @@ from binascii import unhexlify
 
 from ..rpc import get_transaction
 from ..libs.segwit import decode
+from ..exceptions import NetworkError
+from ..config import config
+
+# Bytom config
+config: dict = config()
 
 
-def find_smart_contract_utxo(transaction_id, network):
+def find_smart_contract_utxo(transaction_id: str, network: str = config["network"], vapor: bool = False) -> str:
     """
     Find Bytom smart contract UTXO id by transaction id/hash.
 
@@ -14,25 +19,28 @@ def find_smart_contract_utxo(transaction_id, network):
     :type transaction_id: str
     :param network: Bytom network.
     :type network: str
+    :param vapor: Bytom sidechain vapor, defaults to False.
+    :type vapor: bool
     :returns: str -- UTXO id.
 
     >>> from pybytom.transaction.tools import find_smart_contract_utxo
-    >>> find_smart_contract_utxo("e4d4fab70a41536cf298d6f261c0a1ac169a45be47583f7240115c9059cd0d03", "mainnet")
+    >>> find_smart_contract_utxo("e4d4fab70a41536cf298d6f261c0a1ac169a45be47583f7240115c9059cd0d03", "mainnet", False)
     "169a45be47583f7240115c9059cd0d03e4d4fab70a41536cf298d6f261c0a1ac"
     """
 
     utxo_id = None
     contract_transaction = get_transaction(
-        transaction_id=transaction_id, network=network)
+        transaction_id=transaction_id, network=network, vapor=vapor
+    )
     contract_outputs = contract_transaction["outputs"]
     for contract_output in contract_outputs:
         if contract_output["address"] == "smart contract":
-            utxo_id = contract_output["utxo_id"]
+            utxo_id = contract_output["id"]
             break
     return utxo_id
 
 
-def find_p2wsh_utxo(transaction_id, network):
+def find_p2wsh_utxo(transaction_id: str, network: str = config["network"], vapor: bool = False) -> str:
     """
     Find Bytom segwit pay to script hash UTXO id by transaction id/hash.
 
@@ -40,29 +48,33 @@ def find_p2wsh_utxo(transaction_id, network):
     :type transaction_id: str
     :param network: Bytom network.
     :type network: str
+    :param vapor: Bytom sidechain vapor, defaults to False.
+    :type vapor: bool
     :returns: str -- UTXO id.
 
     >>> from pybytom.transaction.tools import find_p2wsh_utxo
-    >>> find_p2wsh_utxo("0a1ac169a45be47583f72401e4d4fab70a41536cf298d6f261c15c9059cd0d03", "mainnet")
+    >>> find_p2wsh_utxo("0a1ac169a45be47583f72401e4d4fab70a41536cf298d6f261c15c9059cd0d03", "mainnet", False)
     "cd0d03e4d4fab70a41536cf298d6f261c0a1ac169a45be47583f7240115c9059"
     """
 
     utxo_id = None
     if network == "mainnet":
-        hrp = "bm"
+        hrp = "vp" if vapor else "bm"
     elif network == "solonet":
-        hrp = "sm"
+        hrp = "sp" if vapor else "sm"
     elif network == "testnet":
-        hrp = "tm"
+        hrp = "tp" if vapor else "tm"
     else:
-        raise ValueError("invalid network option, choose only mainnet, solonet or testnet network")
+        raise NetworkError(f"Invalid '{network}' network",
+                           "choose only 'mainnet', 'solonet' or 'testnet' networks.")
     contract_transaction = get_transaction(
-        transaction_id=transaction_id, network=network)
+        transaction_id=transaction_id, network=network, vapor=vapor
+    )
     contract_outputs = contract_transaction["outputs"]
     for contract_output in contract_outputs:
         _, address_hash = decode(hrp, contract_output["address"])
         if address_hash is not None and \
                 len(unhexlify(bytearray(address_hash).hex())) == 32:  # deep
-            utxo_id = contract_output["utxo_id"]
+            utxo_id = contract_output["id"]
             break
     return utxo_id
